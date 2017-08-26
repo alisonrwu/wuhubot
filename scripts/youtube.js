b96ad83df5a7b253aff3bc4987fe82b94b0d9451
Alison <alison.wu.0606@gmail.com>
2017-08-25 23:31:25 -0700

    Add Youtube script

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -5,18 +5,54 @@
 //   None
 //
 // Commands:
-//   wuhu jam
+//   wuhu jam - jam to my most recent song obsession
+//   wuhu save me
 // 
 // Author:
 //   alisonrwu
 
+var YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || 'AIzaSyB-dbIR9ikjmnVT38lXveT2GvjaNEOb4so'
+var YOUTUBE_PLAYLIST_ID = 'PLRmmX__GO3AhdxGuBxnrkOvnjz5TcxjNg'
+var YOUTUBE_BASE_VIDEO = 'https://www.youtube.com/watch?v=';
+
 module.exports = function(robot) {
 
-	robot.respond(/jam/i, function(msg){
-		var yt_base_v = 'https://www.youtube.com/watch?v=';
+	robot.respond(/save me/i, function(msg) {
         var save_me = 'GZjt_sA2eso';
 
-        msg.send(yt_base_v + save_me);
+        msg.send(YOUTUBE_BASE_VIDEO + save_me);
+    });
+
+    robot.respond(/jam/i, function(msg) {
+    	var yt_playlistItems = 'https://www.googleapis.com/youtube/v3/playlistItems'
+
+    	var query = {
+    		'key': YOUTUBE_API_KEY,
+    		'maxResults': '50',
+			'part': 'id,contentDetails',
+			'playlistId': YOUTUBE_PLAYLIST_ID
+		};
+
+    	flipPage(null);
+
+		function flipPage(token) {
+			query.pageToken = token;
+			msg.http(yt_playlistItems).query(query).get()(function(err,res,body) {
+				if(err) {
+					msg.send("Error!!!");
+				}
+				var json = JSON.parse(body);
+				if(json.nextPageToken) {
+					flipPage(json.nextPageToken);
+				} else {
+					msg.send(YOUTUBE_BASE_VIDEO + getLastVideoId(json.items));
+				}
+			});
+		}
+
+		function getLastVideoId(videos) {
+			return videos[videos.length-1].contentDetails.videoId;
+		}
     });
 
 }
