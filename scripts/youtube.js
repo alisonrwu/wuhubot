91a8a8f42c413c67e70d9c86314460966b1d34b4
Alison <alison.wu.0606@gmail.com>
2017-08-29 22:27:19 -0700

    add chill

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -5,15 +5,16 @@
 //   None
 //
 // Commands:
-//   wuhu jam - to my most recent song obsession
+//   jam - to my most recent song obsession
+//   chill - to random song of a calm-er selection
 //   save me [alt]
 // 
 // Author:
 //   alisonrwu
 
-var YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || 'AIzaSyB-dbIR9ikjmnVT38lXveT2GvjaNEOb4so'
-var YOUTUBE_PLAYLIST_ID = 'PLRmmX__GO3AhdxGuBxnrkOvnjz5TcxjNg'
-var YOUTUBE_BASE_VIDEO = 'https://www.youtube.com/watch?v=';
+var YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || 'AIzaSyB-dbIR9ikjmnVT38lXveT2GvjaNEOb4so';
+var YOUTUBE_API = 'https://www.googleapis.com/youtube/v3/playlistItems'
+var YOUTUBE_BASE_URL = 'https://www.youtube.com/watch?v=';
 
 module.exports = function(robot) {
 
@@ -25,18 +26,18 @@ module.exports = function(robot) {
         		attachment: {
         			"type": "video",
         			"payload": {
-        				"url": YOUTUBE_BASE_VIDEO + save_me
+        				"url": YOUTUBE_BASE_URL + save_me
         			}
         		}
         	},
         	attachments: true //TODO: can't send to FB GRAPH API endpoint (message attachments).....
         }
 
-        msg.send(YOUTUBE_BASE_VIDEO + save_me);
+        msg.send(YOUTUBE_BASE_URL + save_me);
     });
 
-    robot.respond(/jam/i, function(msg) {
-    	var yt_playlistItems = 'https://www.googleapis.com/youtube/v3/playlistItems'
+    robot.hear(/^(wuhu )?jam/i, function(msg) {
+		var YOUTUBE_PLAYLIST_ID = 'PLRmmX__GO3AhdxGuBxnrkOvnjz5TcxjNg';
 
     	var query = {
     		'key': YOUTUBE_API_KEY,
@@ -49,7 +50,7 @@ module.exports = function(robot) {
 
 		function flipToLastPage(token) {
 			query.pageToken = token;
-			msg.http(yt_playlistItems).query(query).get()(function(err,res,body) {
+			msg.http(YOUTUBE_API).query(query).get()(function(err,res,body) {
 				if(err) {
 					return msg.send("Error!!!");
 				}
@@ -57,13 +58,38 @@ module.exports = function(robot) {
 				if(json.nextPageToken) {
 					flipToLastPage(json.nextPageToken);
 				} else {
-					msg.send(YOUTUBE_BASE_VIDEO + getLastVideoId(json.items) + '&list=' + YOUTUBE_PLAYLIST_ID);
+					msg.send(YOUTUBE_BASE_URL + getLastVideoId(json.items) + '&list=' + YOUTUBE_PLAYLIST_ID);
 				}
 			});
 		}
 
 		function getLastVideoId(videos) {
 			return videos[videos.length-1].contentDetails.videoId;
+		}
+    });
+
+    robot.hear(/^(wuhu )?chill/i, function(msg) {
+    	var CHILL_PLAYLIST_ID = 'PLRmmX__GO3AhR3fpsx3XPHktV3KFyMhhG';
+
+    	var query = {
+    		'key': YOUTUBE_API_KEY,
+    		'maxResults': '50',
+			'part': 'id,contentDetails',
+			'playlistId': CHILL_PLAYLIST_ID
+		};
+
+		msg.http(YOUTUBE_API).query(query).get()(function(err,res,body) {
+			if(err) {
+				return msg.send("Error!!!");
+			}
+			var json = JSON.parse(body);
+			msg.send(YOUTUBE_BASE_URL + getRandomVideoId(json.items) + '&list=' + CHILL_PLAYLIST_ID);
+		});
+
+		function getRandomVideoId(videos) {
+			var max = videos.length-1;
+			var index = Math.floor(Math.random() * max);
+			return videos[index].contentDetails.videoId;
 		}
     });
 
